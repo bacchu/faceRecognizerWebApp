c89922e7108acc9482ef8d1dce49b331eadd5259
Trivikram Prasad <bacchu@gmail.com>
2020-08-22 21:45:40 +0530

    New functionality to display output data in a different html page

--- app.py
+++ app.py
@@ -1,26 +1,44 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
 from werkzeug.utils import secure_filename
+import os
 
 import face_analyzer
 
-face_vector_data = {'Data': 'empty'}
+UPLOAD_FOLDER = '/Users/triv/images/uploads/'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = 'mysecretkey'
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def upload_file():
-    json_image_data = {'data': 'empty'}
+    return render_template('upload.html')
 
+@app.route('/uploaded', methods=['GET', 'POST'])
+def uploaded_file():
     if request.method == 'POST':
-        f = request.files['filefield']
-        f.save('/Users/triv/images/uploads/' + secure_filename(f.filename))
-        image_file = '/Users/triv/images/uploads/' + f.filename
-        json_image_data = face_analyzer.get_data(image_file)
-        # return redirect(url_for('upload_file'))
-    return render_template('index.html', result=json_image_data)
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        f = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if f.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if f and allowed_file(f.filename):
+            filename = secure_filename(f.filename)
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            #f.save('/Users/triv/images/uploads/' + secure_filename(f.filename))
+            image_file = filename
+            json_image_data = face_analyzer.get_data(image_file)
+            return render_template('uploaded.html',result=json_image_data)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
