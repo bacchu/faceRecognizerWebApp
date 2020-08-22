bf1fd8ec651691c3ef488a410082f4fbd4c65687
Trivikram Prasad <bacchu@gmail.com>
2020-08-23 00:31:13 +0530

    Improved error handling and display formatting for non-image files

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ import os
 import face_analyzer
 
 UPLOAD_FOLDER = '/Users/triv/images/uploads/'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+#ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 app.secret_key = 'mysecretkey'
 
 def allowed_file(filename):
@@ -32,13 +34,15 @@ def uploaded_file():
         if f.filename == '':
             flash('No selected file')
             return redirect(request.url)
-        if f and allowed_file(f.filename):
-            filename = secure_filename(f.filename)
+        if f and  allowed_file(f.filename):
             #f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             f.save('/Users/triv/images/uploads/' + secure_filename(f.filename))
             image_file = '/Users/triv/images/uploads/' + secure_filename(f.filename)
             json_image_data = face_analyzer.get_data(image_file)
             return render_template('uploaded.html',result=json_image_data)
+        else:
+            return render_template('uploaded.html',result= f.filename + ': NOT AN IMAGE FILE!')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
