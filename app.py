08d1fa384d77c31b661cbdf5644914434af1cfd4
Trivikram Prasad <bacchu@gmail.com>
2020-08-23 00:42:41 +0530

    Formatting changes

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, flash
 from werkzeug.utils import secure_filename
-import os
 
 import face_analyzer
 
 UPLOAD_FOLDER = '/Users/triv/images/uploads/'
-#ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+# ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 app.secret_key = 'mysecretkey'
 
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 @app.route('/')
 def upload_file():
     return render_template('upload.html')
+
 
 @app.route('/uploaded', methods=['GET', 'POST'])
 def uploaded_file():
@@ -34,14 +36,14 @@ def uploaded_file():
         if f.filename == '':
             flash('No selected file')
             return redirect(request.url)
-        if f and  allowed_file(f.filename):
-            #f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        if f and allowed_file(f.filename):
+            # f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             f.save('/Users/triv/images/uploads/' + secure_filename(f.filename))
             image_file = '/Users/triv/images/uploads/' + secure_filename(f.filename)
             json_image_data = face_analyzer.get_data(image_file)
-            return render_template('uploaded.html',result=json_image_data)
+            return render_template('uploaded.html', result=json_image_data)
         else:
-            return render_template('uploaded.html',result= f.filename + ': NOT AN IMAGE FILE!')
+            return render_template('uploaded.html', result=f.filename + ': NOT AN IMAGE FILE!')
 
 
 if __name__ == '__main__':
